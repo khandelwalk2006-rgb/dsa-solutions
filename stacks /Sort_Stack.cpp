f7cf27f63e7956387ae7876d6a41fc3a68031c2b
khandelwalk2006-rgb <khandelwalk2006@gmail.com>
2026-01-04 19:25:38 +0530

    Create Sort_Stack.cpp

---
+++ stacks /Sort_Stack.cpp
@@ -0,0 +1,36 @@
+#include <stack>
+using namespace std;
+
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
+
+
+void sortedInsert(stack<int> &stack, int num)
+{
+    if(stack.empty() || stack.top() < num)
+    {
+        stack.push(num);
+        return;
+    }
+
+    int n = stack.top();
+    stack.pop();
+
+    sortedInsert(stack, num);
+
+    stack.push(n);
+}
+
+
+void sortStack(stack<int> &stack)
+{
+    if(stack.empty())
+        return;
+
+    int num = stack.top();
+    stack.pop();
+
+    sortStack(stack);
+
+    sortedInsert(stack, num);
+}
