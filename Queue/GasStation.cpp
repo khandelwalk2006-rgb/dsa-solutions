ef47dac3d97c26666ac0cd546117d3f33e2cc946
khandelwalk2006-rgb <khandelwalk2006@gmail.com>
2026-01-12 11:35:48 +0530

    Create GasStation.cpp

---
+++ Queue/GasStation.cpp
@@ -0,0 +1,32 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+  public:
+    int startStation(vector<int> &gas, vector<int> &cost) {
+        
+        int deficit = 0;
+        int balance = 0;
+        int start = 0;
+        
+        int n = gas.size();
+        
+        for(int i = 0; i < n; i++)
+        {
+            balance += gas[i] - cost[i];
+            
+            if(balance < 0)
+            {
+                deficit += balance;
+                start = i + 1;
+                balance = 0;
+            }
+        }
+        
+        if(deficit + balance >= 0) {
+            return start;
+        }
+        
+        return -1;
+    }
+};
